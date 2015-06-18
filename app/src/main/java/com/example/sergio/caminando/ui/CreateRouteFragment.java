61298a1b040561a6ed9e258e067826ad721148fd
sergio <sergio.lucas.lopez@gmail.com>
2015-06-18 11:56:19 +0200

    APP. Add framework to take photo and put in fragment_create_route. Tenemos que utilizar la ruta de la foto para añadir en CreateRouteActivity a los datos del textview para enviear al server FIX-> Desaparece cuando rotate

--- app/src/main/java/com/example/sergio/caminando/ui/CreateRouteFragment.java
+++ app/src/main/java/com/example/sergio/caminando/ui/CreateRouteFragment.java
@@ -2,6 +2,8 @@ package com.example.sergio.caminando.ui;
 
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,27 +12,39 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.sergio.caminando.R;
 import com.example.sergio.myapplication.backend.domain.conference.model.ConferenceForm;
 import com.fourmob.datetimepicker.date.DatePickerDialog;
 import com.google.api.client.util.DateTime;
+import com.kbeanie.imagechooser.api.ChooserType;
+import com.kbeanie.imagechooser.api.ChosenImage;
+import com.kbeanie.imagechooser.api.ImageChooserListener;
+import com.kbeanie.imagechooser.api.ImageChooserManager;
 import com.sleepbot.datetimepicker.time.RadialPickerLayout;
 import com.sleepbot.datetimepicker.time.TimePickerDialog;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
-
 /**
  * Created by sergio on 30/05/15.
  */
-public class CreateRouteFragment extends Fragment implements com.fourmob.datetimepicker.date.DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+public class CreateRouteFragment extends Fragment implements
+        com.fourmob.datetimepicker.date.DatePickerDialog.OnDateSetListener,
+        TimePickerDialog.OnTimeSetListener,
+        ImageChooserListener {
 
     public static final String DATEPICKER_TAG_START = "datepickerstart";
     public static final String DATEPICKER_TAG_END = "datepickerend";
@@ -54,6 +68,14 @@ public class CreateRouteFragment extends Fragment implements com.fourmob.datetim
     private SimpleDateFormat dateFormatter;
     private DateTime startDate;
     private DateTime endDate;
+
+    //Delarate Views to catch photo route
+    private ImageView imageViewThumbnail;
+    private TextView textViewFile;
+    private ImageChooserManager imageChooserManager;
+    private ProgressBar pbar;
+    private String filePath;
+    private int chooserType;
 
     public interface Callbacks {
         public void uploadRoute(ConferenceForm conference);
@@ -88,6 +110,27 @@ public class CreateRouteFragment extends Fragment implements com.fourmob.datetim
         setHasOptionsMenu(true);
 
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_create_route, container, false);
+
+        Button buttonTakePicture = (Button) root.findViewById(R.id.buttonTakePicture);
+        buttonTakePicture.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                takePicture();
+            }
+        });
+        Button buttonChooseImage = (Button) root.findViewById(R.id.buttonChooseImage);
+        buttonChooseImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+        imageViewThumbnail = (ImageView) root.findViewById(R.id.imageViewThumb);
+        textViewFile = (TextView) root.findViewById(R.id.textViewFile);
+        pbar = (ProgressBar) root.findViewById(R.id.progressBar);
+        pbar.setVisibility(View.GONE);
 
         mRouteName = (EditText) root.findViewById(R.id.route_name_editext);
         mCityName = (EditText) root.findViewById(R.id.route_city_name_editext);
@@ -132,6 +175,9 @@ public class CreateRouteFragment extends Fragment implements com.fourmob.datetim
         }
         dateFormatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.US);
         setDateTimeField();
+
+        // Para ocultar el teclado al abrir lo de crear ruta. Se iba siempre a Route Name
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 
     private void setDateTimeField() {
@@ -222,7 +268,7 @@ public class CreateRouteFragment extends Fragment implements com.fourmob.datetim
         int id = item.getItemId();
 
         //To create route
-        if (id == R.id.menu_create){
+        if (id == R.id.menu_create) {
             createRoute();
         }
 
@@ -252,4 +298,106 @@ public class CreateRouteFragment extends Fragment implements com.fourmob.datetim
         return conferenceForm;
     }
 
+    private void chooseImage() {
+        chooserType = ChooserType.REQUEST_PICK_PICTURE;
+        imageChooserManager = new ImageChooserManager(this,
+                ChooserType.REQUEST_PICK_PICTURE, "myfolder", true);
+        imageChooserManager.setImageChooserListener(this);
+        try {
+            pbar.setVisibility(View.VISIBLE);
+            filePath = imageChooserManager.choose();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void takePicture() {
+        chooserType = ChooserType.REQUEST_CAPTURE_PICTURE;
+        imageChooserManager = new ImageChooserManager(this,
+                ChooserType.REQUEST_CAPTURE_PICTURE, "myfolder", true);
+        imageChooserManager.setImageChooserListener(this);
+        try {
+            pbar.setVisibility(View.VISIBLE);
+            filePath = imageChooserManager.choose();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == getActivity().RESULT_OK
+                && (requestCode == ChooserType.REQUEST_PICK_PICTURE || requestCode == ChooserType.REQUEST_CAPTURE_PICTURE)) {
+            if (imageChooserManager == null) {
+                reinitializeImageChooser();
+            }
+            imageChooserManager.submit(requestCode, data);
+        } else {
+            pbar.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onImageChosen(final ChosenImage image) {
+        getActivity().runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                pbar.setVisibility(View.GONE);
+                if (image != null) {
+                    textViewFile.setText(image.getFilePathOriginal());
+                    imageViewThumbnail.setImageURI(Uri.parse(new File(image
+                            .getFileThumbnail()).toString()));
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onError(final String reason) {
+        getActivity().runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                pbar.setVisibility(View.GONE);
+                Toast.makeText(getActivity(), reason,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    // Should be called if for some reason the ImageChooserManager is null (Due
+    // to destroying of activity for low memory situations)
+    private void reinitializeImageChooser() {
+        imageChooserManager = new ImageChooserManager(this, chooserType,
+                "myfolder", true);
+        imageChooserManager.setImageChooserListener(this);
+        imageChooserManager.reinitialize(filePath);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putInt("chooser_type", chooserType);
+        outState.putString("media_path", filePath);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onViewStateRestored(Bundle savedInstanceState) {
+
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey("chooser_type")) {
+                chooserType = savedInstanceState.getInt("chooser_type");
+            }
+
+            if (savedInstanceState.containsKey("media_path")) {
+                filePath = savedInstanceState.getString("media_path");
+            }
+        }
+        super.onViewStateRestored(savedInstanceState);
+    }
 }
